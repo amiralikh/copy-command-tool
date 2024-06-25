57533535c13094be26d23da8776b381c35743bfc
amiralikhfino <amirali.khoshneviszadeh@finotivegroup.com>
2024-06-25 23:47:04 +0200

    add relations to models

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Feed extends Model
 {
-    use HasFactory;
+    public function instagramSource(): HasOne
+    {
+        return $this->hasOne(InstagramSource::class);
+    }
+
+    public function tiktokSource(): HasOne
+    {
+        return $this->hasOne(TiktokSource::class);
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
